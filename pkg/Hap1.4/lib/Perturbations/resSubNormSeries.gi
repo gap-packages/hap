890594a091044c5410d0c690702c47701590a241
gap <gap@mcs.st-andrews.ac.uk>
2011-12-17 01:12:58 +0000

    Version 1.4, 03/05/2006

---
+++ pkg/Hap1.4/lib/Perturbations/resSubNormSeries.gi
@@ -0,0 +1,32 @@
+#(C) Graham Ellis 2005-2006
+
+#####################################################################
+InstallGlobalFunction(ResolutionSubnormalSeries,
+function(arg)
+local  
+	L,Ltrunc,K,RN,RQ,N,E,Q,EhomQ,gensE,gensQ;
+
+L:=arg[1];
+K:=arg[2];
+
+if Length(L)=1 then return
+ResolutionFiniteGroup(L[1],K);
+fi;
+
+Ltrunc:=List([1..Length(L)-1],i->L[i]);
+
+RN:=ResolutionSubnormalSeries(Ltrunc,K);
+N:=RN.group;
+E:=L[1];
+EhomQ:=NaturalHomomorphismByNormalSubgroup(E,N);
+Q:=Image(EhomQ);
+gensE:=GeneratorsOfGroup(E);
+gensQ:=List(gensE,x->Image(EhomQ,x));
+
+RQ:=ResolutionFiniteGroup(gensQ,K);
+
+
+return ResolutionFiniteExtension(gensE,gensQ,RQ,K,false,RN);
+end);
+#####################################################################
+
---
+++ pkg/Hap1.4/lib/hap.gd
@@ -0,0 +1,105 @@
+#(C) Graham Ellis, 2005-2006.
+
+## FREE G MODULES ###################################################
+DeclareGlobalFunction("Negate");
+DeclareGlobalFunction("NegateWord");
+DeclareGlobalFunction("AlgebraicReduction");
+DeclareGlobalFunction("AddWords");
+DeclareGlobalFunction("AddWordsModP");
+DeclareGlobalFunction("PrintZGword");
+DeclareGlobalFunction("TietzeReduction");
+DeclareGlobalFunction("MultiplyWord");
+DeclareGlobalFunction("WordModP");
+
+## NONABELIAN TENSOR ################################################
+DeclareGlobalFunction("NonabelianTensorSquare");
+DeclareGlobalFunction("ThirdHomotopyGroupOfSuspensionB");
+DeclareGlobalFunction("NonabelianExteriorProduct");
+DeclareGlobalFunction("RelativeSchurMultiplier");
+DeclareGlobalFunction("Epicentre");
+
+## RESOLUTIONS ######################################################
+DeclareGlobalFunction("ResolutionFiniteGroup");
+DeclareGlobalFunction("ResolutionSmallFpGroup");
+DeclareGlobalFunction("PresentationOfResolution");
+DeclareGlobalFunction("ResolutionFiniteSubgroup");
+DeclareGlobalFunction("ResolutionSubgroup");
+DeclareGlobalFunction("ResolutionAsphericalPresentation");
+DeclareGlobalFunction("ResolutionAbelianGroup");
+DeclareGlobalFunction("ResolutionAlmostCrystalGroup");
+DeclareGlobalFunction("ResolutionAlmostCrystalQuotient");
+
+## RESOLUTIONS MOD P ################################################
+DeclareGlobalFunction("ResolutionPrimePowerGroup");
+
+## FUNCTORS #########################################################
+DeclareGlobalFunction("EvaluateProperty");
+DeclareGlobalFunction("EvenSubgroup");
+DeclareGlobalFunction("EquivariantChainMap");
+DeclareGlobalFunction("TensorWithIntegers");
+DeclareGlobalFunction("TensorWithIntegersModP");
+DeclareGlobalFunction("PrimePartDerivedFunctor");
+DeclareGlobalFunction("ReduceGenerators");
+DeclareGlobalFunction("HomToIntegers");
+DeclareGlobalFunction("HomToIntegralModule");
+DeclareGlobalFunction("PermToMatrixGroup");
+DeclareGlobalFunction("AbelianInvariantsToTorsionCoefficients");
+DeclareGlobalFunction("TorsionGeneratorsAbelianGroup");
+DeclareGlobalFunction("TensorWithRationals");
+DeclareGlobalFunction("BigStepLCS");
+
+
+## PERTURBATIONS ####################################################
+DeclareGlobalFunction("TwistedTensorProduct");
+DeclareGlobalFunction("ResolutionFiniteExtension");
+DeclareGlobalFunction("ResolutionNormalSeries");
+DeclareGlobalFunction("ResolutionDirectProduct");
+DeclareGlobalFunction("ResolutionExtension");
+DeclareGlobalFunction("ResolutionFiniteDirectProduct");
+DeclareGlobalFunction("ResolutionSubnormalSeries");
+
+
+
+## ARTIN COXETER ####################################################
+DeclareGlobalFunction("CoxeterDiagramMatrix");
+DeclareGlobalFunction("CoxeterDiagramVertices");
+DeclareGlobalFunction("CoxeterDiagramFpArtinGroup");
+DeclareGlobalFunction("CoxeterDiagramFpCoxeterGroup");
+DeclareGlobalFunction("CoxeterSubDiagram");
+DeclareGlobalFunction("CoxeterDiagramComponents");
+DeclareGlobalFunction("CoxeterDiagramDegree");
+DeclareGlobalFunction("CoxeterDiagramIsSpherical");
+DeclareGlobalFunction("ResolutionArtinGroup");
+
+## HOMOLOGY #########################################################
+DeclareGlobalFunction("Homology");
+DeclareGlobalFunction("IntegralHomology");
+DeclareGlobalFunction("ModularHomology");
+DeclareGlobalFunction("GroupHomology");
+DeclareGlobalFunction("IntegralCohomology");
+DeclareGlobalFunction("Cohomology");
+DeclareGlobalFunction("Syzygy");
+DeclareGlobalFunction("CR_IntegralCycleToClass");
+DeclareGlobalFunction("CocycleCondition");
+DeclareGlobalFunction("StandardCocycle");
+DeclareGlobalFunction("IsSuperperfect");
+
+## RINGS ############################################################
+DeclareGlobalFunction("CR_IntegralCohomology");
+DeclareGlobalFunction("CR_ChainMapFromCocycle");
+DeclareGlobalFunction("CR_CocyclesAndCoboundaries");
+DeclareGlobalFunction("CR_IntegralClassToCocycle");
+DeclareGlobalFunction("CR_IntegralCocycleToClass");
+DeclareGlobalFunction("IntegralCupProduct");
+DeclareGlobalFunction("IntegralRingGenerators");
+
+## CURVATURE ########################################################
+DeclareGlobalFunction("IsAspherical");
+DeclareGlobalFunction("PolytopalGenerators");
+DeclareGlobalFunction("VectorStabilizer");
+DeclareGlobalFunction("PolytopalComplex");
+
+## POLYCYLIC ########################################################
+DeclareGlobalFunction("ResolutionAbelianPcpGroup");
+DeclareGlobalFunction("ResolutionNilpotentGroup");
+
