4e2943a6fa8958a7273cfcb6c4a9dd655290c474
Graham Ellis <graham.ellis@nuigalway.ie>
2012-06-21 09:03:52 +0100

    Version 1.10.8, 19/06/2012

---
+++ lib/TorsionSubcomplexes/TorsionSubcomplexes.gd
@@ -0,0 +1,6 @@
+DeclareGlobalFunction("IsPnormal");
+DeclareGlobalFunction("TorsionSubcomplex");
+DeclareGlobalFunction("VisualizeTorsionSkeleton");
+DeclareGlobalFunction("ReduceTorsionSubcomplex");
+DeclareGlobalFunction("DisplayAvailableCellComplexes");
+
